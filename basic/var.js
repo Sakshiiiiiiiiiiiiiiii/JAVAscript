147e04c0a8dc6edd254eac090087c2be42262889
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-04-12 20:56:05 +0000

    container config done

---
+++ basic/var.js
@@ -0,0 +1,13 @@
+const accoundId=32243
+let lname="sakshi"
+var accountcity="jay"
+accountpass="2345"
+console.log(accoundId);
+
+//let lname ="manshi" //SyntaxError: Identifier 'lname' has already been declared
+ accountcity="ayo"
+accountpass="63456"
+// TypeError: Assignment to constant variable.
+// accoundId=7367
+let result;
+console.table([accoundId, lname ,accountcity, accountpass,result])
