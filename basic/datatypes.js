532d618f095417f6c138f4077d639e3b4706179b
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-04-13 08:13:32 +0000

    datatypes

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -5,5 +5,22 @@ console.log(3
     +3
 ) // correct but not readable
 
- 
+ // datatypes
+    //numbres=>2^53
+    //bigint
+    //string=>""
+    //boolean=> true/false
+    //null=>standlone value => define the empty value
+    //undefine=>when value not define
+    //symbol=>unique
+//non-premitive datatype
+    //object
+
+    let fname="saskkkkk" //tell var type
+    console.log(typeof fname);
+    console.log(typeof null); //object
+    console.log(typeof undefine); //undefine
+    
+    
+
 
