37acc0b24f9e5850d83321d3ea450f0e8e1a3d66
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-04-14 07:19:39 +0000

    my excersise

---
+++ basic/dataypes-summary.js
@@ -0,0 +1,31 @@
+// two type of data according store and access
+// 1. primitive
+  //7types : string,number,boolean,null,undefine,symbole, bigInt
+   
+const score=199
+const scoreValue=25435.4
+let userName;
+const isActive=false
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+console.log(id===anotherId) // false;
+
+// 2. reference type(non-primitive)
+//array,object,function
+
+// 1. array
+const fruits=['apple','banana','orange']
+{
+  let myobject=
+ { name:'apple',
+  color:'red'
+  }
+}
+
+//typeoff 
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+
+ 
---
+++ basic/operation.js
@@ -0,0 +1,19 @@
+// console.log("1"+2);
+// console.log(1+"2");
+
+// console.log(true);// print true
+// console.log(+true);//print 1
+// console.log(true+); //error
+
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+console.log(x);
+console.log(y);
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 3n
+console.log(x);
+console.log(y);
+
