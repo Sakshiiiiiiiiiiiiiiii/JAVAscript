e6947772b29763db6b642f658759b508faa56287
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-13 13:44:30 +0000

    up

---
+++ basic/numsandmathe.js
@@ -0,0 +1,15 @@
+const score=400;
+console.log(score);
+const balance=new Number(12220);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(4));
+
+// const otherName=123.7678;
+// console.log(otherName.toPrecision(4));
+
+// const hund=10000000000
+// console.log(hund.toLocaleString('en-in'))
+console.log(Math.random()) // between 0 and 1
+
