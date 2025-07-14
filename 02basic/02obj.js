944af4a20643dd0d00ae5f3850129f4f53685d8b
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-14 15:30:02 +0000

    d

--- 02basic/02obj.js
+++ 02basic/02obj.js
@@ -63,9 +63,53 @@ const obj3 = Object.assign({}, obj1, obj2);
 }
 */
 
+/*
+
 console.log(Object.keys(obj3)); // ['name', 'age', 'location']
 console.log(Object.values(obj3)); // ['Bob', 30, 'Seattle']
 console.log(Object.entries(obj3)); // [['name', 'Bob'], ['age', 30], ['location', 'Seattle']]
 console.log(obj3.hasOwnProperty('name')); // true
 console.log(obj3.hasOwnProperty('lastname')); // false
  
+*/
+
+const course = {
+    title: "JavaScript Basics",
+    duration: "4 weeks",
+    topics: ["Variables", "Functions", "Objects", "Arrays"],
+    instructor: {
+        name: "John Smith",
+        experience: "5 years"
+    }
+};
+ //console.log(course.instructor.name); // John Smith
+ 
+const {instructor}= course;
+// console.log(instructor.name); // John Smith
+
+const {title, duration, topics} = course;
+// console.log(title); // JavaScript Basics
+// console.log(duration); // 4 weeks
+// console.log(topics); // ['Variables', 'Functions', 'Objects', 'Arrays']
+
+//object destructuring
+const {instructor: {name, experience}} = course;
+// console.log(name); // John Smith
+// console.log(experience); // 5 years
+
+//object destructuring with default values
+const {title: courseTitle, duration: courseDuration, topics: courseTopics = []} = course;
+// console.log(courseTitle); // JavaScript Basics
+// console.log(courseDuration); // 4 weeks
+// console.log(courseTopics); // ['Variables', 'Functions', 'Objects', 'Arrays']
+
+//API
+//json
+// {
+//     name: "John Doe",
+//     age: 30,
+//     location: "New York",
+// }
+
+
+
