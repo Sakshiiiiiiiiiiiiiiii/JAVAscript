2a75cd6730f722b64938c7f3e3d58b479a79da34
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-16 13:57:14 +0000

    d

---
+++ 03basic/jsec.js
@@ -0,0 +1,5 @@
+// javascript execution context
+// This file is part of the JavaScript execution context for the project.
+// It is used to set up the environment for running JavaScript code.
+
+// first part of the context global execution context
