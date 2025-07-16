2c5863267cf8b452b597413870cef2c4740ccc62
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-16 09:52:35 +0000

    done

--- 03basic/fun.js
+++ 03basic/fun.js
@@ -35,14 +35,14 @@ const num3=add(5,6) //11
  console.log("result : " + num3); //result : undefined
 
 
-function add2(num1,num2)//parameter
+function add2(num1,num2)//parameter 
  {
     // let res=num1+num2
     // return res
     return num1+num2 
 }
 const num4=add2(5,9)
-// console.log("result : " + res);// error                  
+// console.log("result : " + res);// error             
 console.log("result : " + num4); //result : 14 
 
 function loginuser(username) 
@@ -52,4 +52,62 @@ function loginuser(username)
 
 console.log(loginuser("sakshi"));
 // ******console.log(loginuser()); //output undefined just logged in
-//
+
+
+function calculatecartprice(...num1)//rest parameter
+{
+    return num1;
+}
+
+console.log(calculatecartprice(100)); //100
+console.log(calculatecartprice(100,4,400,356));
+
+//[ 100, 4, 400, 356 ]
+
+function calculatecartprice(val1,val2, ...num1)// rest parameter with default parameter
+{
+    return num1;
+}
+console.log(calculatecartprice(100,4,400,356)); //[ 400, 356 ]
+console.log(calculatecartprice(100,4)); //[]
+
+const user={
+    name:"sakshi",
+    age:22,
+    isloggedin:true,
+    lastactivitytime:"yesterday"
+}
+
+function userinfo(anyobject) 
+{
+    console.log(`Name is ${anyobject.name} and age is ${anyobject.age}`);
+    console.log(`User logged in status is ${anyobject.isloggedin}`);
+    console.log(`Last activity time is ${anyobject.lastactivitytime}`);
+}
+userinfo(user);
+//output
+// Name is sakshi and age is 22 
+// User logged in status is true
+// Last activity time is yesterday
+
+userinfo(
+    {
+        name:"manshi",
+        age:18,
+        isloggedin:false,
+        lastactivitytime:"today"
+    });
+
+    //output
+// Name is s and age is 22
+// User logged in status is true
+// Last activity time is yesterday
+
+const myNewArray=[1,2,3,4,5,6,7,8,9,10];
+function secondValue(getArray)
+{
+    return getArray[1];
+}
+
+console.log(secondValue(myNewArray)); //2
+console.log(secondValue([100,200,300,400])); //200
