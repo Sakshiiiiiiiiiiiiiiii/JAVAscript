b7724e961537eddbb1430f642b435d95749a5479
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-17 12:13:41 +0000

    continew

--- 05iteration/loop.js
+++ 05iteration/loop.js
@@ -6,6 +6,7 @@ for (let i = 0; i < 11; i++) {
 //output: 0,1,2,3,4,5,6,7,8,9,10
 
 //nested for loop
+/*
 for (let i = 0; i < 3; i++) {
     console.log(`Outer loop iteration: ${i}`);
     
@@ -16,6 +17,8 @@ for (let i = 0; i < 3; i++) {
         
     }
 }
+*/
+
 //output:
 //Outer loop iteration: 0
 //i: 0, j: 0
@@ -39,4 +42,25 @@ for (let i = 0; i < 3; i++) {
 //i: 2, j: 2
 //Inner loop iteration: 2 and outer loop iteration: 2
 
+let myArray = [1, 2, 4, 5];
+//for of loop
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+    
+}
+//output: 1,2,3,4,5
 
+//break and continue
+for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        // console.log("Breaking at 5");
+        // break; // exits the loop when i is 5
+        continue; // skips the iteration when i is 5
+    }
+    console.log('the value of i is: ' + i);
+
+    
+}
+//output for break: 0,1,2,3,4
+//output for continue: 0,1,2,3,4,6,7,8,9
