af6a699631d6c5793e0c78acea816e92186af2b3
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-17 10:05:25 +0000

    ok

--- 04controlFlow/one.js
+++ 04controlFlow/one.js
@@ -1,3 +1,13 @@
-if () {
-    
+// already clear
+
+if (2==="2") {
+    console.log("write");   
 }
+
+//shorthand notation 
+
+const blance=1000
+//if (blance>500) console.log("test"),console.log("test2");
+
+ 
+
