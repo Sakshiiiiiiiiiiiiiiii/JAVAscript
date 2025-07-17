8967f351b0444146ad09cf0d1dea829db1f12bfc
Sakshiiiiiiiiiiiiiiii <manshi4usakshi@gmail.com>
2025-07-17 10:53:00 +0000

    d

--- 04controlFlow/truthy.js
+++ 04controlFlow/truthy.js
@@ -5,6 +5,7 @@ if (userEmail) {
     
 }else
 {
+    //
     console.log("manshi");
     
 }
@@ -14,4 +15,20 @@ if (userEmail) {
 //truthy value=> 
 //           "0" ,'false' ," ",[],{},function(){}
 
-//
+// nullish coalescing operator(??); null , undefine
+ let val1;
+// val1=5??10  
+//val1=null??23
+val1=undefined??564
+ console.log(val1);
+ 
+
+ //ternary operator
+
+ //condition?true:false
+ const iceteap=100
+ iceteap <=80? console.log("less than 80") : console.log("more than 80")
+
+ //outpu
+ 
+ 
